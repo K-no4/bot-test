62cdad23343f237b08a4b4cd6df4b0d8a3d0fa55
K-no4 <51031653+K-no4@users.noreply.github.com>
2019-10-23 18:46:55 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -13,9 +13,15 @@ function respond()
           postAnouncement();
           this.res.end();
           break;
+        case '/help':
+          this.res.writeHead(200);
+          postHelp();
+          this.res.end();
+          break;
         default:
         console.log("unkown response");
         this.res.writeHead(200);
+        postError();
         this.res.end();
 
       }
@@ -82,6 +88,78 @@ function postAnouncement(){
 
 
   botResponse = "UT-Austin\r\n  https://www.facebook.com/events/392382481673389/";
+
+    options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function posthelp(){
+  var botResponse, options, body, botReq;
+
+
+  botResponse = "This bot responds to comands in the format of /'text' \r\n There currently two comands: \r\n /help - gives list of commands \r\n /t - gives information about the latest tournament";
+
+    options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function posthelp(){
+  var botResponse, options, body, botReq;
+
+
+  botResponse = "Unkown command use /help for list of commands";
 
     options = {
     hostname: 'api.groupme.com',
