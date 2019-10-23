d00b598ba88f8f41c1935e9c47cd2ff6b79a09a7
K-no4 <waabee.geshow@gmail.com>
2019-10-23 17:53:43 -0500

    edit to

--- bot.js
+++ bot.js
@@ -54,4 +54,40 @@ function postMessage() {
 }
 
 
+
+function postAnouncement(){
+  var botResponse, options, body, botReq;
+
+
+  botResponse = "UT-Austin\r\n  https://www.facebook.com/events/392382481673389/";
+
+    options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 exports.respond = respond;
