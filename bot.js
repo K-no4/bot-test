eb41407963e7e8bde7ca6eac0d9467aa5ae371da
K-no4 <51031653+K-no4@users.noreply.github.com>
2019-10-25 13:08:50 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -113,7 +113,7 @@ function postCringe(txt){
     switch (txt)
     {
       case 1:
-        botResponse="words 1";
+        botResponse="Nice";
       break;
       case 2:
         botResponse="words 2";
@@ -150,8 +150,52 @@ function postCringe(txt){
   });
   botReq.end(JSON.stringify(body));
 }
+/*
+function postImage(txt){
+  var txt, botResponse, options, body, botReq;
 
 
+    switch (txt)
+    {
+      case 1:
+        botResponse="Nice";
+      break;
+      case 2:
+        botResponse="words 2";
+      break;
+      default:
+      break;
+    }
+    options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
 
+  body = {
+    "bot_id" : botID,
+    "image" : botResponse
+  };
+  console.log()
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+*/
 
 exports.respond = respond;
