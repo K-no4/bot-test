6a2728b15bee37335bca55e56a8579bc8adb212d
K-no4 <51031653+K-no4@users.noreply.github.com>
2019-10-26 00:19:12 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,59 +2,38 @@ var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
-
 function respond() {
-  var msg, request = JSON.parse(this.req.chunks[0]),
+  var dv, msg, str, request = JSON.parse(this.req.chunks[0]);
+
   msg=request.text;
+  dv=request.userid;
+  msg=msg.toLowerCase();
+  console.log(dv);
+  console.log(msg);
       switch (msg){
         case '/t':
           this.res.writeHead(200);
-          postAnouncement();
+          postInfo(1);
           this.res.end();
           break;
-        case '/help':
+        case "/h":
           this.res.writeHead(200);
-          postHelp();
-          this.res.end();
+          postInfo(2);
+          this.res.end(200);
           break;
-          case '69':
+        case "69":
           this.res.writeHead(200);
           postCringe(1);
           this.res.end();
           break;
-          case 'no':
+        case 'no':
           this.res.writeHead(200);
           postCringe(2);
           this.res.end();
           break;
-          case 'Hey boss':
-          this.res.writeHead(200);
-          postCringe(3);
-          this.res.end(200);
-          break;
-          case 'fuck you':
-          this.res.writeHead(200);
-          postCringe(4);
-          this.res.end(200);
-          break;
-          case 'Fuck you':
-          this.res.writeHead(200);
-          postCringe(4);
-          this.res.end(200);
-          break;
-          case 'Fuck You':
-          this.res.writeHead(200);
-          postCringe(4);
-          this.res.end(200);
-          break;
-          case 'Hello':
+        case 'hello':
           this.res.writeHead(200);
           postCringe(5);
-          this.res.end(200);
-          break;
-          case 'Destroy Humanity';
-          this.res.writeHead(200);
-          postCringe(6);
           this.res.end(200);
           break;
         default:
@@ -63,49 +42,63 @@ function respond() {
         this.res.end();
 
       }
-}
-
-function postAnouncement(){
-  var botResponse, options, body, botReq;
 
 
-  botResponse = "UT-Austin\r\n  https://www.facebook.com/events/392382481673389/";
-
-    options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
+      if (msg.indexOf("hey")>=0){
+        console.log(true);
+           if (msg.indexOf("boss")>=0){
+          console.log(true);
+          this.res.writeHead(200);
+          postCringe(3);
+          this.res.end(200);
+        }
+          else{}
       }
-  });
+      else{}
+    
+      if (msg.indexOf("fuck")>=0){
+        console.log(true);
+           if (msg.indexOf("you")>=0){
+          console.log(true);
+            if (msg.indexOf("no")<0){
+            this.res.writeHead(200);
+            postCringe(4);
+            this.res.end(200);
+          }
+            else{}
+        }
+          else{}
+      }
+      else{} 
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
+      if (msg.indexOf("destroy")>=0){
+        console.log(true);
+           if (msg.indexOf("humanity")>=0){
+          console.log(true);
+          this.res.writeHead(200);
+          postCringe(6);
+          this.res.end(200);
+        }
+          else{}
+      }
+      else{
+        console.log(false);
+      }   
+
 }
 
-function postHelp(){
-  var botResponse, options, body, botReq;
+function postInfo(txt){
+  var botResponse, options, body, botReq, txt;
 
 
-  botResponse = "This bot responds to comands in the format of /'text' \r\n There currently two comands: \r\n /help - gives list of commands \r\n /t - gives information about the latest tournament";
+  switch(txt){  
+    case 1:
+    botResponse = 'UT-Austin\r\n  https://www.facebook.com/events/392382481673389/';
+    break;
+    case 2:
+    botResponse = 'This bot responds to comands in the format of /{text}\r There currently two comands:\r /h - gives list of commands\r /t - gives information about the latest tournament';
+    break;
+  }
 
     options = {
     hostname: 'api.groupme.com',
@@ -140,7 +133,6 @@ function postHelp(){
 function postCringe(txt){
   var txt, botResponse, options, body, botReq;
 
-
     switch (txt)
     {
       case 1:
@@ -153,7 +145,7 @@ function postCringe(txt){
         botResponse="can I habe a pizza please";
         break;
       case 4:
-        botResponse="No fuck you.";
+        botResponse="No fuck you";
         break;
       case 5:
         botResponse="Hello Human";
@@ -193,52 +185,5 @@ function postCringe(txt){
   });
   botReq.end(JSON.stringify(body));
 }
-/*
-function postImage(txt){
-  var txt, botResponse, options, body, botReq;
-
-
-    switch (txt)
-    {
-      case 1:
-        botResponse="Nice";
-      break;
-      case 2:
-        botResponse="words 2";
-      break;
-      default:
-      break;
-    }
-    options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "image" : botResponse
-  };
-  console.log()
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
-*/
 
 exports.respond = respond;
